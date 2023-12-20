72d41153e2ef78a256e205ef0d23cbf6460954f4
mark <markjmitry@gmail.com>
2023-12-20 04:11:22 +0000

    add colors

--- clogger.py
+++ clogger.py
@@ -6,15 +6,44 @@ import sys
 import os
 
 
+class ColorFormatter(logging.Formatter):
+    def __init__(self, *args: str, **kwargs: str:)
+        super().__init__(*args, **kwargs)
+        
+        GREY = "\x1b[31;20m"
+        YELLOW = "\x1b[33;20m"
+        RED = "\x1b[36;20m"
+        BOLD_RED = "\x1b[36;1m"
+        
+        self.colors = {
+            logging.DEBUG: GREY,
+            logging.INFO: GREY,
+            logging.WARNING: YELLOW,
+            logging.ERROR: BOLD_RED,
+            logging.CRITICAL: BOLD_RED,
+        }
+
+        self._fmts = {
+            level: self.add_colors(level) for level in self.colors
+        }
+
+    def add_colors(self, level: int) -> str:
+        _color = self.colors.get(level)
+        RESET = "\x1b[0m"
+        return self._style._fmt.replace('#c', _color).replace('#r', RESET)
+
+    def format(self, record: int) -> str:
+        self._style._fmt = self._fmts[record.levelno] 
+        return super().format(record)
+
+
 def log(
         level: str = 'INFO',
-        logdir: str = '.',
+        logdir: str = '.logs',
         max_bytes: int = 524288,
         backup_count: int = 2,
+        logger_name: str = __name__,
     ) -> logging.Logger:
-    """
-    simple logger using python's logging module and RotatingFileHandler
-    """
     
     loglevel = {
         'DEBUG': logging.DEBUG,
@@ -26,16 +55,20 @@ def log(
 
     try:
         level = loglevel[level.upper()]
-    except KeyError as e:
-        _err = f'invalid loglevel {e}: using "INFO"'
+    except (KeyError, AttributeError) as e:
+        _err = f'invalid level {e}: using default value "INFO" instead'
         level = logging.INFO
     else:
         _err = None
 
-    logger = logging.getLogger(__name__)
+    logger = logging.getLogger(logger_name)
     logger.setLevel(level)
-    formatter = logging.Formatter(
-        fmt='{asctime}.{msecs:03.0f}| ({module}) {levelname}: {message}',
+    ftime = '{asctime:15s}'
+    module = '{module:7s}'
+    clevel = '#c{levelname:8s}#r'
+    message = '{message}'
+    formatter = ColorFormatter(
+        fmt=f'{ftime}| {module} {clevel} | {message}',
         datefmt='%Y-%m-%d %H:%M:%S',
         style='{'
    )
@@ -54,9 +87,7 @@ def log(
     console_handler = logging.StreamHandler(sys.stdout)
     console_handler.setFormatter(formatter)
     logger.addHandler(console_handler)
-
     if _err is not None:
         logger.warning(f'{_err}. level options: {list(loglevel.keys())}')
-
     return logger
 
